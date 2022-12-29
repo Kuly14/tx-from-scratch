013cc4dc122b75584e35659fa5293eb90a44d806
David Kulman <kulman.david@gmail.com>
2022-12-29 04:27:26 +0100

    format

--- examples/deploy.rs
+++ examples/deploy.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 use ethereum_types::{H160, H256};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::{fmt::Write, num::ParseIntError, str::FromStr};
 use web3::types::Bytes;
 
@@ -10,10 +10,9 @@ use jsonrpsee::rpc_params;
 
 use tx_from_scratch::Transaction;
 
-
 #[tokio::main]
 pub async fn main() {
-    // Deploy bytecode of the contract 
+    // Deploy bytecode of the contract
     // You can get this with remix or hardhat
     let data = "608060405234801561001057600080fd5b5060b68061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063cc80f6f314602d575b600080fd5b60336047565b604051603e91906067565b60405180910390f35b6000600a905090565b6000819050919050565b6061816050565b82525050565b6000602082019050607a6000830184605a565b9291505056fea26469706673582212209b541ed574700ac76c591bec0d31d371ca1203903669cfb5c59334ea8952ed9564736f6c63430008110033";
 
